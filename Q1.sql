a766c3e1efd83238a94f134e4f93abb4a48de87f
Sameed Jawaid <97083003+sameedj@users.noreply.github.com>
2023-07-26 19:25:52 -0700

    Create Q1.sql

---
+++ Q1.sql
@@ -0,0 +1,13 @@
+-- Q1: The Fed raised interest rates in March 2022, with the data going back to September 2022 in the data set. Was there an Increasing/Decreasing trend in funding?
+
+SELECT
+(SELECT
+ROUND(AVG(valuation), 2)
+FROM vc.data
+WHERE date BETWEEN '2021-07-01' AND '2022-02-30'
+) AS pre_avg_hike_valuation
+,
+ROUND(AVG(valuation), 2) AS post_avg_hike_valuation
+FROM vc.data
+WHERE date BETWEEN '2022-03-01' AND '2022-09-30'
+;
